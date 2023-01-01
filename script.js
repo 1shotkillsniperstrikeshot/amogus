979ad8cb5a5d585b3b959dff08c904a62aaed887
amogus <86213279+1shotkillsniperstrikeshot@users.noreply.github.com>
2023-01-01 10:25:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let query = document.querySelector('.query');
 let searchbtn = document.querySelector('.searchbtn');
 
 searchbtn.onclick = function(){
-    let url = 'https://www.google.com/search?q=' + query.value;
+    let url = 'https://tiki.com/search?q=' + query.value;
     window.open(url,'_self');
 }
 
@@ -12,7 +12,7 @@ function handle(e){
     if(e.keyCode === 13){
         e.preventDefault();
 
-        let url = 'https://www.google.com/search?q=' + query.value;
+        let url = 'https://tiki.com/search?q=' + query.value;
         window.open(url,'_self');
     }
 }
